9c26c12b0db14cb2a7cdb6cff2c1d5bc4f722ec0
basfom <jose.follert@sansano.usm.cl>
2016-11-03 21:11:16 -0300

    New setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+import subprocess
+
+iptables = open("/etc/sysconfig/iptables", "r")
+config = False
+for linea in iptables:
+    if "EcoLab" in linea:
+        config = True
+iptables.close()
+if not config:
+    iptables = open("/etc/sysconfig/iptables", "a")
+    iptables.write("""
+# EcoLabit
+-A INPUT -p tcp --dport 5678 -j ACCEPT")
+""")
+iptables.close()
+
+subprocess.call(["service", "iptables", "restart"])
