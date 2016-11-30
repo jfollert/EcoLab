b9da33862f41bd053289ee9f6e8cccb70804aef5
basfom <jose.follert@sansano.usm.cl>
2016-11-29 22:24:33 -0300

    New arg system

---
+++ argprocess.py
@@ -0,0 +1,82 @@
+import argparse
+import sys
+
+class ArgsProcessClient:
+    def __init__(self, namef):
+        self.__namef = namef
+        parser = argparse.ArgumentParser(usage = self.__namef + """ <command> [<args>]
+        
+Commands:
+    check       Return the status of the machines.
+    add         Adds a machine to the service.
+    remove      Removes a machine to the service.
+    wake        Turn on a machine.
+    down        Turn off a machine.
+    sethour     Sets the power on/off time.
+    """)
+        parser.add_argument('command', help='Subcommand to run')
+        args = parser.parse_args(sys.argv[1:2])
+
+        if not hasattr(self, args.command):
+            print """
+[ERROR] UNRECOGNIZED COMMAND ("""+ sys.argv[1]+""")!
+            """
+            print "use: " + __file__ + " -h\n"
+            exit(1)
+
+        getattr(self, args.command)()
+
+    def check(self):
+        parser = argparse.ArgumentParser(description='Return the status of the machines.', usage = self.__namef + " check [-a] [-i IP/MAC]")
+        parser.add_argument('-a', action='store_true', help= "Return a list of the machines registered with their status.")
+        parser.add_argument('-i', help="Return the status (ON/OFF) of a specific machine.")
+        args = parser.parse_args(sys.argv[2:])
+        if args.a:
+            print "Checking all machines..."
+        if args.i:
+            print "Checking a machine " + args.i + "..."
+            print "Sending Magic Packet..."
+
+
+    def add(self):
+        parser = argparse.ArgumentParser(description='Adds a machine to the service.', usage = self.__namef + " add [IP:MAC]")
+        parser.add_argument('ip_mac')
+        args = parser.parse_args(sys.argv[2:])
+        print "Adding the machine " + args.ip_mac
+
+    def remove(self):
+        parser = argparse.ArgumentParser(description='Removes a machine to the service.', usage = self.__namef + " remove [IP:MAC]")
+        parser.add_argument('ip_mac')
+        args = parser.parse_args(sys.argv[2:])
+        print "Removing the machine " + args.ip_mac
+
+    def wake(self):
+        parser = argparse.ArgumentParser(description='Turn on a machine.', usage = self.__namef + " wake [-a] [-i]")
+        parser.add_argument('-a', action='store_true', help= "Turn on all the machines registered.")
+        parser.add_argument('-i', help="Turn on a specific machine.")
+        args = parser.parse_args(sys.argv[2:])
+        if args.a:
+            print "Awaking all machines..."
+        if args.i:
+            print "Awaking a machine " + args.i + "..."
+
+    def down(self):
+        parser = argparse.ArgumentParser(description='Turn off a machine.', usage = self.__namef + " down [-a] [-i]")
+        parser.add_argument('-a', action='store_true', help= "Turn off all the machines registered.")
+        parser.add_argument('-i', help="Turn off a specific machine.")
+        args = parser.parse_args(sys.argv[2:])
+        if args.a:
+            print "Turning off all machines..."
+
+        if args.i:
+            print "Turning off a machine " + args.i + "..."
+
+    def sethour(self):
+        parser = argparse.ArgumentParser(description='Sets the power on/off time.', usage = self.__namef + " sethour [-w] [-d]")
+        parser.add_argument('-w', help= "Sets the power on time.")
+        parser.add_argument('-d', help="Sets the power off time.")
+        args = parser.parse_args(sys.argv[2:])
+        if args.w:
+            print "Setting power on time to: " + args.w
+        if args.d:
+            print "Setting power off time to: " + args.d
