69f6029156a8b01c0c45bc36f7b3d820da75c7cc
ShadowDogger <shadowdogger2882@gmail.com>
2019-10-03 20:53:26 -0500

    Made ecoserver.py pep-8 compliant

--- ecoserver.py
+++ ecoserver.py
@@ -1,28 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Modules #
+
 import socket
 import sys
 import time
 import subprocess
 
 # Creates a TCP/IP socket
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Binds the socket to the port
-server_address = ("0.0.0.0", 5678)
+
+server_address = ('0.0.0.0', 5678)
 sock.bind(server_address)
 
 # Listens for incoming connections
+
 sock.listen(1)
-sock , (host_c,puerto_c) = sock.accept()
+(sock, (host_c, puerto_c)) = sock.accept()
 while True:
-        cmd  = sock.recv(100000)# recibimos 100000 bytes de datos
-                                #(EN) Receives 100000 bytes of data
+    cmd = sock.recv(100000)  # recibimos 100000 bytes de datos
+    # (EN) Receives 100000 bytes of data
 
-        output = subprocess.Popen(cmd, shell=True, bufsize=100000, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,close_fds=True)# habrimos un nuevo proceso, el mismo seria el que recibimos con recv y guardamos en  cmd
-        #(EN) Starts a new process, received by recv and saved in cmd
+    output = subprocess.Popen(
+        cmd,
+        shell=True,
+        bufsize=100000,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        close_fds=True,
+        )
 
-        out = output.stdout.read() #leemos la salida de dicho proceso
-                                   #(EN) Reads the output of the process
+    # Inicia un nuevo proceso, recibido por recv y guardado en cmd
+    # (EN) Starts a new process, received by recv and saved in cmd
 
-        sock.send(out)#enviamos la salida del proceso al cliente
-                      #(EN) Sends the process output to the client
+    out = output.stdout.read()  # leemos la salida de dicho proceso
+    # (EN) Reads the output of the process
+
+    sock.send(out)  # enviamos la salida del proceso al cliente
+    # (EN) Sends the process output to the client
